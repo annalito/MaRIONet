393ba7382a0f8d6c3dde31c7d04950db433fa74c
Anna Lito Michala <amichala@unimak.dcs.gla.ac.uk>
2016-12-14 16:59:00 +0000

    Working mySQL insert

--- register.php
+++ register.php
@@ -1,7 +1,13 @@
 <?php
 
 include('config.php');
-   
+
+if (!function_exists('boolval')) {
+        function boolval($val) {
+                return (bool) $val;
+        }
+}
+
 $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 if ($db->connect_errno) {
     echo "Failed to connect to MySQL: " . $db->connect_error;
@@ -9,24 +15,30 @@ if ($db->connect_errno) {
 }
 
 
-echo "Dear ", $_POST['name'] , "Thank you for registering to the upcoming MaRIONet event";
+echo "Dear ", $_POST['name'], "<br>";
+echo "Thank you for registering to the upcoming MaRIONet event<br>";
+
+$users_name = $_POST['name'];
+$users_email = $_POST['email'];
+$users_affiliation = $_POST['affiliation'];
+$users_quest = $_POST['panelquestion'];
+$users_request = $_POST['reqs'];
 
 $checkbox = 0;
 
-if ($_POST['attend'].is(":checked")) {
-  $checkboc = 1;
+if($_POST['dinner'] == 'attend') {
+  echo "and for joining us for dinner<br>";
+  $checkbox = 1;
 }
 
-$db->query('INSERT INTO 
-              event20160111 
-              (name, email, affiliation, question, requirements, dinner)
-            VALUES 
-              (\''.$_POST['name'].'\',
-              \''.$_POST['email'].'\', 
-              \''.$_POST['affiliation'].'\', 
-              \''.$_POST['panelquestion'].'\',
-              \''.$_POST['reqs']'\',
-              \''.$checkbox'\'
-            )');
+$db->query('INSERT INTO event20160111 (id, name, email, affiliation, question, requirements, dinner)
+            VALUES (NULL,
+                    \''.$db->escape_string($users_name).'\',
+                    \''.$db->escape_string($users_email).'\', 
+                    \''.$db->escape_string($users_affiliation).'\', 
+                    \''.$db->escape_string($users_quest).'\',
+                    \''.$db->escape_string($users_request).'\',
+                    \''.boolval($checkbox).'\')
+            ');
 ?>
 
