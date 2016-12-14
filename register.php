0e7dd1388e555cd6f896760e45bec8555ef32e59
root <root@46.235.225.72>
2016-12-14 12:55:36 +0000

    setup db and basic connection

--- register.php
+++ register.php
@@ -1,4 +1,10 @@
 <?php
+
+   define ( 'DB_HOST', 'localhost' );
+   define ( 'DB_USER', 'marionet' );
+   define ( 'DB_PASSWORD', 'kGK-6jRa' );
+   define ( 'DB_DATABASE', 'marionetdb' ); 
+
    echo "Hello ", $_POST['name'];
 ?>
 
