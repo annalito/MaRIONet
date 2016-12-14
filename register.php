6239167a1141a8377fd3ad29648552b61771923f
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2016-12-14 18:46:01 +0000

    updated styling and final message after submit

--- register.php
+++ register.php
@@ -1,3 +1,62 @@
+<html>
+<head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>MaRIONet : the UK Manycore Network</title>
+
+<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.1/build/pure-min.css">
+    
+        <!--[if lte IE 8]>
+            <link rel="stylesheet" href="http://purecss.io/combo/1.18.13?/css/layouts/side-menu-old-ie.css">
+        <![endif]-->
+        <!--[if gt IE 8]><!-->
+            <link rel="stylesheet" href="http://purecss.io/combo/1.18.13?/css/layouts/side-menu.css">
+        <!--<![endif]-->
+    <!--[if lt IE 9]>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<div id="layout">
+
+   <!-- Menu toggle -->
+    <a href="#menu" id="menuLink" class="menu-link">
+        <!-- random icon -->
+        <span></span>
+    </a>
+
+
+    <div id="menu">
+        <div class="pure-menu">
+            <a class="pure-menu-heading" href="#">Menu</a>
+
+            <ul class="pure-menu-list">
+                <li class="pure-menu-item">
+                  <a href="index.html" class="pure-menu-link">Home</a>
+                </li>
+                <li class="pure-menu-item">
+                  <a href="mailto:jeremy.singer@glasgow.ac.uk" class="pure-menu-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
+
+<div id="main">
+
+<div class="header">
+    <img class="pure-img" src="marionet_logo.png" alt="marionet logo">
+  </div>
+
+<div class="content">
+
+<h2 class="content-subhead">Registration Complete</h2>
+
+<p>
 <?php
 
 include('config.php');
@@ -16,7 +75,7 @@ if ($db->connect_errno) {
 
 
 echo "Dear ", $_POST['name'], "<br>";
-echo "Thank you for registering to the upcoming MaRIONet event<br>";
+echo "Thank you for registering for the MaRIONet launch event on Wednesday 11 JJanuary in Imperial College London. We look forward to meeting you! Please let other researchers know about this event and encourage them to sign up too.<br>";
 
 $users_name = $_POST['name'];
 $users_email = $_POST['email'];
@@ -42,3 +101,18 @@ $db->query('INSERT INTO event20160111 (id, name, email, affiliation, question, r
             ');
 ?>
 
+</p>
+  
+</div>
+
+</div> <!-- main -->
+</div>
+
+
+  
+<script src="http://purecss.io/combo/1.18.13?/js/ui.js"></script>
+
+</body>
+
+
+</html>
